d88d5265eee529c21982c6fad4c2d42105adfb33
maciejdragan <dragan.maciej@gmail.com>
2026-01-07 11:58:02 -0500

    catalog debug

--- app/test_catalog.py
+++ app/test_catalog.py
@@ -19,25 +19,21 @@ from readers.market_catalog.instrument_query import InstrumentQuery
 def _ms_to_utc_str(ms: int) -> str:
     return datetime.fromtimestamp(ms / 1000.0, tz=timezone.utc).strftime("%Y-%m-%d %H:%M:%S UTC")
 
-
-
 #%%
 cat = MarketCatalog.default()
-
-
 cat.refresh(scan_days=7, all_time=False, use_snapshot=True)
-
 
 #%%
 dfm = cat.markets_df()
 dfi = cat.instruments_df()
 
 #%%
-q = InstrumentQuery.from_catalog(cat).venues("polymarket").cadence_in("4h").underlying_in("ETH")
+q = InstrumentQuery.from_catalog(cat).venues("polymarket").cadence_in("4h").underlying_in("BTC")
 df, ims = q.df_and_items(top_n=10, sort_by='expiration_ms', descending=True)
 
-
-
+df
 #%%
-ob = OrderbookHistory.from_instrument(ims[0])
-ob.levels_df().mid.plot()
+print(ims[0])
+# %%
+print('first seen:', _ms_to_utc_str(ims[0].first_seen_ms))
+print('last seen:', _ms_to_utc_str(ims[0].last_seen_ms))
